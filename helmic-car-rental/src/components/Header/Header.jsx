854f7c672b6d31c3a50dc0d0c0a472b34c5167c5
stella <stellahjames200@gmail.com>
2024-08-23 16:04:37 +0300

    Initial commit

--- helmic-car-rental/src/components/Header/Header.jsx
+++ helmic-car-rental/src/components/Header/Header.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 
+import { Container, Row, Col } from "reactstrap"
+
+
 const Header = () => {
-  return (
-    <div>
-      
+  return <header className="header">
+
+    <div className="header_top">
+      <Container>
+        <Row>
+          <Col lg='6' md='6' sm='6' >
+          <div className="header_top_left">
+            <span>Need Help?</span>
+              <span className="header_top_help">
+              <i class="ri-phone-fill"></i>
+              </span>
+          </div>
+          </Col>
+          <Col lg='6' md='6' sm='6' >
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
+
+  </header>
 }
 
 export default Header
