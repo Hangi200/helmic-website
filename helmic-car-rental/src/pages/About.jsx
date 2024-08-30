f78dc08b3f017c3dc00036753b49b8619a5e240e
stella <stellahjames200@gmail.com>
2024-08-30 15:15:38 +0300

    Initial commit

--- helmic-car-rental/src/pages/About.jsx
+++ helmic-car-rental/src/pages/About.jsx
@@ -1,11 +1,79 @@
-import React from 'react'
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import AboutSection from "../components/UI/AboutSection";
+import { Container, Row, Col } from "reactstrap";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+
+import driveImg from "../assets/all-images/driver.jpeg";
+import OurMembers from "../components/UI/OurMembers";
+import "../styles/about.css";
 
 const About = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <Helmet title="About">
+      <CommonSection title="About Us" />
+      <AboutSection aboutClass="aboutPage" />
 
-export default About
+      <section className="about__page-section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section_title">
+                  Our Vision
+                </h2>
+
+                <p className="section__description">
+                From the very day it was established, Helmic Car Rental has a clear vision: that of
+continuously developing this company into a premier one-stop car rental service
+boasting international standards.
+                </p>
+
+                <p className="section__description">
+                Because of this devote commitment, the
+company is now able to offer useful advice on strategic vehicle planning to best
+suit the exacting requirements of many different types of company.
+                </p>
+
+                <div className=" d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section_subtitle">Need Any Help?</h6>
+                    <h4>+00123456789</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <BecomeDriverSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+              <h6 className="section__subtitle">Experts</h6>
+              <h2 className="section__title">Our Members</h2>
+            </Col>
+            <OurMembers />
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default About;
