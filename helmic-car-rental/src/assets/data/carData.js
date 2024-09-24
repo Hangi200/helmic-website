c633d676b5caf076fddd08691dc922b37321b605
stella <stellahjames200@gmail.com>
2024-09-24 13:25:38 +0300

    Initial commit

--- helmic-car-rental/src/assets/data/carData.js
+++ helmic-car-rental/src/assets/data/carData.js
@@ -3,27 +3,34 @@ import img01 from "../all-images/cruiser1.png";
 import img02 from "../all-images/coaster.png";
 import img03 from "../all-images/harrier.png";
 import img04 from "../all-images/fortuner.png";
-import img05 from "../all-images/toyota.png";
+import img05 from "../all-images/RAV4.jpeg";
 import img06 from "../all-images/toyota-hiace.png";
-import img07 from "../all-images/coster-car.jpeg";
+import img07 from "../all-images/hiace.png";
 import img08 from "../all-images/alphard.jpg";
 import img09 from "../all-images/land-cruiser.png";
+import img10 from "../all-images/150prado-car.png";
+import img11 from "../all-images/vanguardcar.png";
+import img12 from "../all-images/range.png";
+import img13 from "../all-images/discovery.png";
+import img14 from "../all-images/prado120.png";
+import img15 from "../all-images/hillux-car.png";
+
 
 const carData = [
   {
     id: 1,
     brand: "Toyota",
     rating: 112,
-    carName: "Toyota pickup",
+    carName: "v8-vxr",
     imgUrl: img01,
-    model: "model-150",
-    price: 200000,
+    model: "model-2022",
+    price: 700000,
     speed: "20kmpl",
     gps: "GPS Navigation",
     seatType: "Heated seats",
     automatic: "Automatic",
     description:
-      " Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam. Dolor labore lorem no accusam sit justo sadipscing labore invidunt voluptua, amet duo et gubergren vero gubergren dolor. At diam.",
+      "",
   },
 
   {
@@ -32,7 +39,7 @@ const carData = [
     rating: 102,
     carName: "Toyota Coster",
     imgUrl: img02,
-    model: "Model-2022",
+    model: "Model-2002",
     price: 280000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -48,7 +55,7 @@ const carData = [
     rating: 132,
     carName: "Harrier",
     imgUrl: img03,
-    model: "Model-2022",
+    model: "Model-2017",
     price: 120000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -64,7 +71,7 @@ const carData = [
     rating: 102,
     carName: "Fortuner",
     imgUrl: img04,
-    model: "Model-2022",
+    model: "Model-2019",
     price: 250000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -80,7 +87,7 @@ const carData = [
     rating: 94,
     carName: "Toyota RAV4",
     imgUrl: img05,
-    model: "Model-2013",
+    model: "Model-2010",
     price: 120000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -112,7 +119,7 @@ const carData = [
     rating: 82,
     carName: "Toyota Hiace",
     imgUrl: img07,
-    model: "Model 3",
+    model: "Model-2010",
     price: 180000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -128,7 +135,7 @@ const carData = [
     rating: 52,
     carName: "Toyota Alphad",
     imgUrl: img08,
-    model: "Model 3",
+    model: "Model-2019                                                       ",
     price: 250000,
     speed: "20kmpl",
     gps: "GPS Navigation",
@@ -143,8 +150,99 @@ const carData = [
     rating: 52,
     carName: "Toyota Land Cruiser",
     imgUrl: img09,
-    model: "Model 3",
+    model: "Model-2016",
     price: 500000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+
+  {
+    id: 10,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Prado-150",
+    imgUrl: img10,
+    model: "Model-2016",
+    price: 220000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+  {
+    id: 11,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Vanguard",
+    imgUrl: img11,
+    model: "Model-2010",
+    price: 120000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+  {
+    id: 12,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Range",
+    imgUrl: img12,
+    model: "Model-2016",
+    price: 500000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+  {
+    id: 13,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Discovery",
+    imgUrl: img13,
+    model: "Model-2016",
+    price: 500000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+  {
+    id: 14,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Prado-120",
+    imgUrl: img14,
+    model: "Model-2021",
+    price: 500000,
+    speed: "20kmpl",
+    gps: "GPS Navigation",
+    seatType: "Heated seats",
+    automatic: "Automatic",
+    description:
+      "",
+  },
+  {
+    id: 15,
+    brand: "Toyota",
+    rating: 52,
+    carName: "Hillux",
+    imgUrl: img15,
+    model: "Model-2016",
+    price: 350000,
     speed: "20kmpl",
     gps: "GPS Navigation",
     seatType: "Heated seats",
